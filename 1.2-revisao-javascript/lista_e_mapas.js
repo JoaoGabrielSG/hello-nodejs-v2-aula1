6088bbb8b22d95e3f3dd40d3f40dd507676decdd
JoaoGabrielSG <joaogabriel@MacBook-Pro-de-joao.local>
2016-11-10 16:53:14 -0300

    add List_and_maps

---
+++ 1.2-revisao-javascript/lista_e_mapas.js
@@ -0,0 +1,30 @@
+var lista = [];
+
+lista.push(4, 2, "terra", 40, "perigo");
+
+console.log(lista);
+
+lista.pop();
+
+lista.shift();
+
+console.log(lista);
+
+lista.unshift(4);
+
+console.log(lista.length);
+
+mapa = {a:1, b:2, c:3, d:4, e:5};
+
+for(let x in mapa){
+	console.log(x)
+}
+
+for(let y in mapa){
+	console.log(mapa[y])
+}
+
+console.log("a"in mapa);
+console.log("u" in mapa);
+
+
