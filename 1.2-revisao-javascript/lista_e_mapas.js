cd6fff5756840eac8b0449d13514868ea48f49ad
JoaoGabrielSG <joaogabriel@MacBook-Pro-de-joao.local>
2016-11-13 14:04:43 -0300

    add map function

--- 1.2-revisao-javascript/lista_e_mapas.js
+++ 1.2-revisao-javascript/lista_e_mapas.js
@@ -14,6 +14,14 @@ lista.unshift(4);
 
 console.log(lista.length);
 
+var nova_lista = lista.map(function(numero){
+	return numero * 3;
+});
+
+console.log("nova_lista: ", nova_lista);
+
+
+
 mapa = {a:1, b:2, c:3, d:4, e:5};
 
 for(let x in mapa){
